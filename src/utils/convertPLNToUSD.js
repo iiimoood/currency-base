c2aa2bdbcde5999d82c8eebc681a0f1c78c52385
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-07-02 09:44:07 +0200

    Add tests for function convertPLNToUSD

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,22 @@
+export const convertPLNToUSD = (PLN) => {
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+};
